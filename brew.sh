f1c16aa31cd14846c13e104b359f8ee80fadccfb
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-06-10 19:25:13 +0100

    Moved it to a better place

--- brew.sh
+++ brew.sh
@@ -13,11 +13,3 @@ brew bundle
 
 # Cleanup outdated versions from Cellar
 brew cleanup
-
-# Homebrew’s installed location.
-BREW_PREFIX=$(brew --prefix)
-
-# symlink dart sdk to Flutter bin
-if [[ ! -d "$HOME/.source/flutter_sdk/bin/cache/dart-sdk" ]]; then
-  ln -sf "${BREW_PREFIX}/opt/dart/libexec" "$HOME/.source/flutter_sdk/bin/cache/dart-sdk"
-fi
