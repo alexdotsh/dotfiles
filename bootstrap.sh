d8d263249a3baa35c8b17b54bcfa22dac742c032
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-06-30 21:53:36 +0100

    Some improvements

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,19 @@
 
 echo -e "Start bootstrapping.. \U1F3C1"
 
-if [[ ! -d "${HOME}/.source" ]]; then
-  git clone --recursive https://github.com/alexmirkhaydarov/dotfiles.git "${HOME}/.source/code"
+WORKDIR="${HOME}/workspace"
+export WORKDIR
+
+if [[ ! -d "${WORKDIR}" ]]; then
+  echo "Cloning dotfiles"
+
+  git clone --recursive https://github.com/alexmirkhaydarov/dotfiles.git "${WORKDIR}/code"
 fi
 
 # Check for Homebrew and then install if not found
 if /bin/test ! "$(which brew)"; then
+  echo "Installing Homebrew"
+
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
@@ -17,6 +24,8 @@ source brew.sh
 
 # Download Prezto and configure if not found
 if [[ ! -d "${HOME}/.zprezto" ]]; then
+  echo "Cloning prezto and initializing"
+
   # Install and configure default Prezto configuration framework
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
@@ -29,7 +38,9 @@ fi
 
 # Download Vundle if .vim directory is not found
 if [[ ! -d "${HOME}/.vim" ]]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}"/.vim/bundle/Vundle.vim
+  echo "Cloning Vundle and installing plugins"
+
+  git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
 
   # Install Plugins
   vim +PluginInstall +qall
@@ -39,28 +50,30 @@ fi
 BREW_PREFIX=$(brew --prefix)
 
 # Istall Flutter SDK
-if [[ ! -d "${HOME}/.source/flutter_sdk" ]]; then
-  git clone https://github.com/flutter/flutter.git -b stable "${HOME}/.source/flutter_sdk"
+if [[ ! -d "${WORKDIR}/flutter_sdk" ]]; then
+  echo "Cloning Flutter and setting up Dart"
+
+  git clone https://github.com/flutter/flutter.git -b stable "${WORKDIR}/flutter_sdk"
 
   # symlink dart sdk to Flutter bin
-  ln -sf "${BREW_PREFIX}/opt/dart/libexec" "$HOME/.source/flutter_sdk/bin/cache/dart-sdk"
+  ln -sf "${BREW_PREFIX}/opt/dart/libexec" "${WORKDIR}/flutter_sdk/bin/cache/dart-sdk"
 fi
 
 echo -e "Symlinking.. \U1F517"
 DOTFILES_DIR="${PWD}"
 
 # Start the symlink
-ln -sf "${DOTFILES_DIR}/functions/zsh_private" "${HOME}"/.zsh_private
-ln -sf "${DOTFILES_DIR}/functions/prompt_garrett_setup" "${ZDOTDIR:-$HOME}"/.zprezto/modules/prompt/functions/prompt_garrett_setup
-ln -sf "${DOTFILES_DIR}/.vimrc" "${HOME}"/.vimrc
-ln -sf "${DOTFILES_DIR}/.tmux.conf" "${HOME}"/.tmux.conf
-ln -sf "${DOTFILES_DIR}/.gitconfig" "${HOME}"/.gitconfig
+ln -sf "${DOTFILES_DIR}/functions/zsh_private" "${HOME}/.zsh_private"
+ln -sf "${DOTFILES_DIR}/functions/prompt_garrett_setup" "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions/prompt_garrett_setup"
+ln -sf "${DOTFILES_DIR}/.vimrc" "${HOME}/.vimrc"
+ln -sf "${DOTFILES_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
+ln -sf "${DOTFILES_DIR}/.gitconfig" "${HOME}/.gitconfig"
 
-if grep -Fxq "[ -r ~/.zsh_private ] && source ~/.zsh_private" "${HOME}"/.zshrc
+if grep -Fxq "[ -r ~/.zsh_private ] && source ~/.zsh_private" "${HOME}/.zshrc"
 then
   echo ''
 else
-  echo '[ -r ~/.zsh_private ] && source ~/.zsh_private' >> "${HOME}"/.zshrc
+  echo '[ -r ~/.zsh_private ] && source ~/.zsh_private' >> "${HOME}/.zshrc"
 fi
 
 echo -e "Finished bootstrapping.. \U1F3AD"
