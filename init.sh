fd66929108ed0aeb6026e7fafd1fa6bafccbf8dd
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2021-10-29 23:28:27 +0100

    removed nonascii char
    Signed-off-by: Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>

--- init.sh
+++ init.sh
@@ -6,5 +6,5 @@ DOTFILES_DIR="${WORKDIR}/code/dotfiles"
 if [[ ! -d "${WORKDIR}" ]]; then
   echo "Cloning dotfiles"
 
-  git clone -–depth 1 https://github.com/alexmirkhaydarov/dotfiles.git "${DOTFILES_DIR}"
+  git clone --depth 1 https://github.com/alexmirkhaydarov/dotfiles.git "${DOTFILES_DIR}"
 fi
